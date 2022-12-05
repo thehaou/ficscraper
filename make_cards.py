68eab7a4bf1bd0d7b07686e8c6abb34d056c7069
thehaou <ludibrumsea@gmail.com>
2022-12-04 22:50:56 -0800

    use mako for card templating

--- make_cards.py
+++ make_cards.py
@@ -1,12 +1,19 @@
 from html2image import Html2Image
+from mako.template import Template
 
 if __name__ == '__main__':
+    mako_template = Template(filename='card_templates/top_5_fandom.html')
+    rendered_template = mako_template.render(items=[
+        {'name': 'Batman - All Media Types', 'details': '526 fics over 7,450,240 words'}
+    ])
     hti = Html2Image(output_path="card_output",
-                     size=(180, 320))  # size: (width, height)
+                     size=(360, 640))  # size: (width, height)
 
     print("Screenshotting...")
     hti.screenshot(
-        html_file='card_templates/card.html', css_file='card_templates/card.css',
+        # html_file='card_templates/top_5_fandom.html',
+        html_str=rendered_template,
+        css_file='card_templates/card.css',
         save_as='test.jpg'
     )
     print("Done")
