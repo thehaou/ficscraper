16a2a34e7d083b733274b3dd3ca354e731b015b4
thehaou <ludibrumsea@gmail.com>
2022-12-09 19:45:26 -0800

    testing out cards... top 5 looks acceptable...

--- make_cards.py
+++ make_cards.py
@@ -1,19 +1,36 @@
 from html2image import Html2Image
 from mako.template import Template
+from mako.exceptions import RichTraceback
+
+
 
 if __name__ == '__main__':
-    mako_template = Template(filename='card_templates/top_5_fandom.html')
-    rendered_template = mako_template.render(items=[
-        {'name': 'Batman - All Media Types', 'details': '526 fics over 7,450,240 words'}
-    ])
-    hti = Html2Image(output_path="card_output",
-                     size=(360, 640))  # size: (width, height)
+    try:
+        mako_template = Template(filename='card_templates/top_5_fandom.html')
+        rendered_template = mako_template.render(
+            title="My Statistic Title",
+            ranked_items=[{'name': 'Top Rank', 'details': 'The best'},
+                          {'name': 'Second Rank', 'details': 'Overshadowed'},
+                          {'name': 'Third Rank', 'details': 'Middle of the pack'},
+                          {'name': 'Fourth Rank', 'details': 'At least they\'re not last'},
+                          {'name': 'Fifth Rank', 'details': 'Happy to be on the list'}])
+    except:
+        traceback = RichTraceback()
+        for (filename, lineno, function, line) in traceback.traceback:
+            # print("File %s, line %s, in %s" % (filename, lineno, function))
+            print(line)
 
-    print("Screenshotting...")
-    hti.screenshot(
-        # html_file='card_templates/top_5_fandom.html',
-        html_str=rendered_template,
-        css_file='card_templates/card.css',
-        save_as='test.jpg'
-    )
-    print("Done")
+    with open('card_templates/test.html', 'w') as f:
+        f.write(rendered_template)
+
+    # hti = Html2Image(output_path="card_output",
+    #                  size=(360, 640))  # size: (width, height)
+    #
+    # print("Screenshotting...")
+    # hti.screenshot(
+    #     # html_file='card_templates/top_5_fandom.html',
+    #     html_str=rendered_template,
+    #     css_file='card_templates/card.css',
+    #     save_as='test.jpg'
+    # )
+    # print("Done")
