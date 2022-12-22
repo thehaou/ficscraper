1b46b455bcfd0eed4751cfcc3ef39aa25174d1bb
thehaou <ludibrumsea@gmail.com>
2022-12-21 19:08:18 -0800

    ...and the actual progress bar

---
+++ src/scrapers/ao3/utils_progress_bar.py
@@ -0,0 +1,24 @@
+import logging
+
+
+def log_progress_bar(prefix='Progress: ', current=0, length=1):
+    # Obviously if length is 10,000 or something we can't render that. The average terminal window
+    # is 80col x 24row; let's use 50 col.
+    bar_length = 50
+
+    # Meat of the bar
+    percent = current/length
+    num_blocks = int(percent * bar_length)
+    num_dash = bar_length - num_blocks
+    blocks = "█" * num_blocks
+    dashes = "-" * num_dash
+
+    # End of the bar
+    suffix = "| {:.2f}%".format(percent * 100)
+
+    # Print it out with print. I'd use logger, but getting logger to print carriage return only is painful
+    print("\r" + prefix + "|" + blocks + dashes + suffix, end="", flush=True)
+
+    # Print newline if done
+    if current == length:
+        print()
