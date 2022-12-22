95edb7ca7bcc69873801ceee7d6d7d9063b80d3a
thehaou <ludibrumsea@gmail.com>
2022-12-21 19:07:55 -0800

    add progress bar, fix incorrect character grabbing (1 vs n), better logging

--- src/scrapers/ao3/bookmark_scraper.py
+++ src/scrapers/ao3/bookmark_scraper.py
@@ -10,8 +10,9 @@ from time import mktime
 from time import sleep
 from bs4 import BeautifulSoup
 
-from src.scrapers.ao3.utils_archive_warnings import get_quadrant, ArchiveWarning
-import src.scrapers.ao3.constants as constants
+from scrapers.ao3.utils_archive_warnings import get_quadrant, ArchiveWarning
+import scrapers.ao3.constants as constants
+from scrapers.ao3.utils_progress_bar import log_progress_bar
 
 
 def error_and_quit(error_msg, input_string=""):
@@ -32,13 +33,13 @@ class AO3BookmarkScraper:
 
         # Set up active session for AO3. This section is based off of
         # https://github.com/alexwlchan/ao3/blob/master/src/ao3/users.py
+        logging.info('Setting up an active session with AO3...')
         sess = requests.Session()
         req = sess.get(self._homepage_url)
         soup = BeautifulSoup(req.text, features='html.parser')
         sleep(1.5)  # Try to avoid triggering rate limiting
 
         authenticity_token = soup.find('input', {'name': 'authenticity_token'})['value']
-        logging.debug('auth token is ' + str(authenticity_token))
 
         req = sess.post(self._login_url, params={
             'authenticity_token': authenticity_token,  # csrf token
@@ -52,13 +53,23 @@ class AO3BookmarkScraper:
                 'Error logging in to AO3; is your password correct?')
 
         self._sess = sess
-        logging.debug('AO3 session set up')
+        logging.info('AO3 session set up')
 
     def __repr__(self):
         # This line is directly from # https://github.com/alexwlchan/ao3/blob/master/src/ao3/users.py
         return '%s(username=%r)' % (type(self).__name__, self._username)
 
     def grab_pages(self):
+        logging.info('Figuring out how many pages to scrape.\n'
+                     '\tAO3 rate-limits roughly ~25 bookmark page-fetches per ~10 minutes.\n'
+                     '\tIf you have a lot of bookmarks, this may take a while. For example:\n'
+                     '\n'
+                     '\t200 pages of bookmarks\n'
+                     '\t= (200pgs) * (10min/25pgs)\n'
+                     '\t= at least 80min\n'
+                     '\n'
+                     '\tYou may need to change your computer sleep settings so it doesn\'t\n'
+                     '\tfall asleep while ficscraper is working.\n')
         # Happily AO3 starts returning blank results if you exceed the max page
         api_url = (self._bookmarks_url_template % self._username)
 
@@ -67,55 +78,108 @@ class AO3BookmarkScraper:
         # END UNCOMMENT SECTION
 
         pages = []
-        for page_num in itertools.count(start=1):
-            # UNCOMMENT FOR TESTING
-            # page_count_limit -= 1
-            # if page_count_limit == 0:
-            #     break
-            # END UNCOMMENT SECTION
+        try:
+            for page_num in itertools.count(start=1):
+                # UNCOMMENT FOR TESTING
+                # page_count_limit -= 1
+                # if page_count_limit == -1:
+                #     break
+                # END UNCOMMENT SECTION
 
-            logging.info('searching on page ' + str(page_num))
+                logging.info('Searching page {} for content'.format(page_num))
 
-            while True:
-                req = self._sess.get(api_url % page_num)
-                sleep(1.5)  # Try to avoid triggering rate limiting
+                count_503 = 0
+                while True:
+                    req = self._sess.get(api_url % page_num)
+                    sleep(1.5)  # Try to avoid triggering rate limiting
 
-                if req.status_code == 429:
-                    # https://otwarchive.atlassian.net/browse/AO3-5761
-                    logging.error("AO3 rate limits; we will receive 429 \"Too Many Requests\" if we ask for pages "
-                                  "too often. We need to wait for several minutes, sorry!")
-                    logging.info("Sleeping for 3 min")
-                    sleep(60 * 3)
+                    if req.status_code == 429:
+                        # https://otwarchive.atlassian.net/browse/AO3-5761
+                        logging.error("AO3 rate limits; we will receive 429 \"Too Many Requests\" if we ask for pages "
+                                      "too often.\nWe need to wait for several minutes, sorry!")
+                        logging.info("Sleeping for 3 min")
+                        sleep(60 * 3)
+                    elif req.status_code == 503:
+                        # Usually this means AO3 is overloaded, but it could also mean maintenance. AO3 usually returns:
+                        #   Error 503
+                        #   The page was responding too slowly.
+                        #   There are so many people using the archive right now, we can't show your page.
+                        #   Follow <a href="https://twitter.com/ao3_status/">@AO3_Status</a> on Twitter for updates
+                        #   if this keeps happening.
+                        # If we hit 503 multiple times in a row, then we should abandon fic-scraping efforts.
+                        count_503 += 1
+                        logging.error("Hit 503 {} time(s). AO3 is overloaded OR the site is under maintenance."
+                                      .format(count_503))
+                        logging.info("Sleeping for 1 min")
+                        sleep(60 * 1)
+
+                        if count_503 == 5:
+                            logging.error('ficscraper has hit 503 too many times in a row. Abandoning efforts.')
+                            logging.error('Please check what\'s wrong with AO3 at https://twitter.com/ao3_status/.')
+                            exit(-1)
+                    else:
+                        break
+
+                soup = BeautifulSoup(req.text, features='html.parser')
+
+                ol_tag = soup.find('ol', class_='bookmark index group')
+                if not ol_tag:
+                    logging.error("Could not find bookmark index group on soup; something is seriously wrong. soup:")
+                    logging.error(soup)
+                    logging.error("Skipping this page...")
+                    continue
+
+                li_tags = ol_tag.findAll('li', attrs={'class': re.compile('^bookmark blurb group.*')})
+                if len(li_tags) > 0:
+                    pages.append(ol_tag)
                 else:
+                    logging.info('No more bookmarks found, stopping page-grabbing')
                     break
-
-            soup = BeautifulSoup(req.text, features='html.parser')
-
-            ol_tag = soup.find('ol', class_='bookmark index group')
-            if not ol_tag:
-                logging.error("Could not find bookmark index group on soup; something is seriously wrong. soup:")
-                logging.error(soup)
-                logging.error("Skipping this page...")
-                continue
-
-            li_tags = ol_tag.findAll('li', attrs={'class': re.compile('^bookmark blurb group.*')})
-            if len(li_tags) > 0:
-                pages.append(ol_tag)
-            else:
-                logging.info('no more bookmarks found, stopping')
-                break
+        except Exception as e:
+            logging.error('ficscraper ran into an error while doing preliminary page fetching. '
+                          '(Your computer probably fell asleep.) Ficscraper will try to run against '
+                          'the pages it was able to successfully collect.')
+            logging.error(e)
         return pages
 
     def process_bookmarks(self):
-        logging.info('Starting AO3')
+        """
+        Processes all entries on https://archiveofourown.org/users/<user_id>/bookmarks.
+        TODO: does NOT handle series bookmarks (yet). Those are skipped.
+
+        :return: (1) dictionary of further specific kinds of dictionaries (i.e. user tags, work info, authors)
+                 (2) single list (JSON) of the complete bookmark history
+        """
+        logging.info('Starting AO3 bookmark-scraping process')
 
         website = 'Archive of Our Own'
         logging.info('\n')
         logging.info('/ ~~~~~~~~~~~~~~~~~~~~~~~ \\')
         logging.info('|      ArchiveOfOurOwn     |')
         logging.info('\\ ~~~~~~~~~~~~~~~~~~~~~~~ /')
+        logging.info(' |     DO NOT USE AO3     |')
+        logging.info(' |   WHILE THIS PROGRAM   |')
+        logging.info(' |       IS RUNNING       |')
+        logging.info(' \\------------------------/')
 
+        # Grab the bs4 of as many pages as we can
         pages = self.grab_pages()
+        num_pages = len(pages)
+
+        logging.info('\n')
+        logging.info('/ ~~~~~~~~~~~~~~~~~~~~~~~ \\')
+        logging.info('|      ArchiveOfOurOwn     |')
+        logging.info('\\ ~~~~~~~~~~~~~~~~~~~~~~~ /')
+        logging.info(' |  You may now use AO3.  |')
+        logging.info(' |     ficscraper will    |')
+        logging.info(' |    continue to work    |')
+        logging.info(' |   in the background.   |')
+        logging.info(' \\------------------------/')
+
+        # Initialize scraping variables
+        logging.info('Beginning to scrape each collected page. This scraping process does not include wrangling tags, '
+                     'so it will finish relatively quickly. See ficscraper\'s "wrangle" process '
+                     '(./ficscraper.sh --wrangle) for more info on how to automatically wrangle these collected tags.')
         page_num = 1
         total_work_row_list = []
         total_author_row_list = []
@@ -125,9 +189,12 @@ class AO3BookmarkScraper:
         total_user_tag_row_list = []
         total_series_row_list = []
         total_ao3_char_row_list = []
+        bookmarks_json_list = []
         for ol_tag in pages:
             # Bookkeeping
-            logging.info('parsing page ' + str(page_num))
+            log_progress_bar(prefix='Scraping page {}/{}:'.format(page_num, num_pages),
+                             current=page_num,
+                             length=num_pages)
             page_num += 1
 
             # Set up lists
@@ -154,17 +221,45 @@ class AO3BookmarkScraper:
                 if content_rating is not None:
                     content_rating = content_rating.name
 
-                # publish isn't available in the process_header_module unfortunately, only on the individual
-                # work page
+                # publish_epoch_sec sadly isn't available in the process_header_module, only on the individual work page
                 publish_epoch_sec = None
 
-                characters, addn_tags = self.process_tags(li_tag) # TOO MANY on page 3, e.g. https://archiveofourown.org/works/13111908/chapters/29997507
+                # Work tags require a separate network call to wrangle them
+                # (ex "no beta we die like men" --> "Not Beta Read"). To keep scraping down to a reasonable amount of
+                # processing time, tag wrangling is a completely separate process. (See ./ficscraper.sh --wrangle)
+                characters, addn_tags = self.process_tags(li_tag)
 
                 series_ids, series_names = self.process_series(li_tag)
 
                 date_bookmarked, user_tags = self.process_user_bookmark(li_tag)
 
-                # Create postgresql entries
+                # Create (singular) JSON entry
+                series_json = [{"series_id": series_id, "series_name": series_name}
+                               for series_id, series_name in zip(series_ids, series_names)] if series_ids else []
+                bookmarks_json_list.append(
+                    {
+                        "work_id": work_id,
+                        "title": title,
+                        "author_id": author_id,
+                        "author_name": author_name,
+                        "word_count": word_count,
+                        "publish_epoch_sec": publish_epoch_sec,
+                        "update_epoch_sec": update_epoch_sec,
+                        "released_chapters_count": released_chapters_count,
+                        "total_chapters_count": total_chapters_count,
+                        "is_complete": is_complete,
+                        "content_rating": content_rating,
+                        "archive_warnings": [archive_warning.value for archive_warning in archive_warnings],
+                        "fandoms": [fandom for fandom in fandoms],
+                        "series": series_json,
+                        "characters": [character for character in characters],
+                        "additional_tags": [tag for tag in addn_tags],
+                        "user_tags": [tag for tag in user_tags],
+                        "date_bookmarked": date_bookmarked,
+                    }
+                )
+
+                # Create CSV entries
                 work_row_list.append(
                     {
                         "work_id": work_id,
@@ -257,8 +352,8 @@ class AO3BookmarkScraper:
                 logging.debug("pg{} - putting {} characters on the queue".format(page_num, len(char_row_list)))
                 total_ao3_char_row_list.extend(char_row_list)
 
-        logging.info('Exiting AO3')
-        return {
+        logging.info('Scraping process completed. Exiting AO3 bookmark scraper')
+        fics_dict = {
             "works": total_work_row_list,
             "authors": total_author_row_list,
             "fandoms": total_fandom_row_list,
@@ -268,6 +363,15 @@ class AO3BookmarkScraper:
             "series": total_series_row_list,
             "characters": total_ao3_char_row_list
         }
+
+        try:
+            bookmarks_json = json.dumps(bookmarks_json_list)
+        except Exception as e:
+            logging.error('Can\'t translate bookmarks to JSON for some reason; returning raw')
+            logging.error(e)
+            bookmarks_json = bookmarks_json_list
+
+        return fics_dict, bookmarks_json
 
     def process_header_module(self, bookmark):
         # logging.debug('processing header module')
@@ -366,17 +470,16 @@ class AO3BookmarkScraper:
         if tags is None:
             error_and_quit('couldn\'t find tags... actually this is plausible if there are NO tags, but...', header_module)
 
-        characters = tags.find('li', class_='characters')
-        character_list = None
-        if characters is not None:
-            character_list = []
-            for character_child in characters.findAll('a'):
-                character_list.append(character_child.contents[0])
+        characters_lis = tags.findAll('li', class_='characters')
+        character_list = []
+        if characters_lis is not None:
+            for character_li in characters_lis:
+                character_a = character_li.find('a')
+                character_list.append(character_a.contents[0])
 
         other_tags = tags.findAll('li', class_='freeforms')
-        other_tags_list = None
+        other_tags_list = []
         if other_tags:
-            other_tags_list = []
             for li_tag in other_tags:
                 a_tag = li_tag.find('a')
                 fun_id = a_tag.contents[0]
@@ -406,7 +509,7 @@ class AO3BookmarkScraper:
         # logging.debug('processing process_stats module')
         stats = bookmark.find('dl', class_='stats')
         if stats is None:
-            logging.info('This bookmark has probably been deleted, skipping it. Bookmark:')
+            logging.debug('This bookmark has probably been deleted, skipping it. Bookmark:')
             logging.debug(bookmark)
             return None, None, None
 
@@ -414,7 +517,7 @@ class AO3BookmarkScraper:
         if word_count_child is None:
             # USUALLY word count has the class assigned to it, but then sometimes you get weird works like
             # https://archiveofourown.org/series/889014 which show up on bookmarks with no classes on their dd tags
-            logging.info('This bookmark is probably a series, skipping it. Bookmark:')
+            logging.debug('This bookmark is probably a series, skipping it. Bookmark:')
             logging.debug(bookmark)
             return None, None, None
         word_count = re.sub(',', '', word_count_child.contents[0])
